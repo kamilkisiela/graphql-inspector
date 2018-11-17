1a25b0e734dbadbaf70dedfe0711bc59e8d9208d
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-11-17 14:22:19 +0100

    Directive

--- src/changes/directive.ts
+++ src/changes/directive.ts
@@ -1,6 +1,12 @@
-import {GraphQLDirective} from 'graphql';
+import {
+  GraphQLDirective,
+  DirectiveLocationEnum,
+  GraphQLArgument,
+  isNonNullType,
+} from 'graphql';
 
 import {Change, CriticalityLevel} from './change';
+import {safeChangeForInputValue} from 'src/utils/graphql';
 
 export function directiveRemoved(directive: GraphQLDirective): Change {
   return {
@@ -18,5 +24,138 @@ export function directiveAdded(directive: GraphQLDirective): Change {
     },
     message: `Directive '${directive.name}' was added`,
     path: `@${directive.name}`,
+  };
+}
+
+export function directiveDescriptionChanged(
+  oldDirective: GraphQLDirective,
+  newDirective: GraphQLDirective,
+): Change {
+  return {
+    criticality: {
+      level: CriticalityLevel.NonBreaking,
+    },
+    message: `Directive '${oldDirective.name}' description changed from '${
+      oldDirective.description
+    }' to '${newDirective.description}'`,
+    path: `@${oldDirective.name}`,
+  };
+}
+
+export function directiveLocationAdded(
+  directive: GraphQLDirective,
+  location: DirectiveLocationEnum,
+): Change {
+  return {
+    criticality: {
+      level: CriticalityLevel.NonBreaking,
+    },
+    message: `Location '${location}' was added to directive '${
+      directive.name
+    }'`,
+    path: `@${directive.name}`,
+  };
+}
+
+export function directiveLocationRemoved(
+  directive: GraphQLDirective,
+  location: DirectiveLocationEnum,
+): Change {
+  return {
+    criticality: {
+      level: CriticalityLevel.Breaking,
+    },
+    message: `Location '${location}' was removed from directive '${
+      directive.name
+    }'`,
+    path: `@${directive.name}`,
+  };
+}
+
+export function directiveArgumentAdded(
+  directive: GraphQLDirective,
+  arg: GraphQLArgument,
+): Change {
+  return {
+    criticality: {
+      level: isNonNullType(arg.type)
+        ? CriticalityLevel.Breaking
+        : CriticalityLevel.NonBreaking,
+    },
+    message: `Argument '${arg.name}' was added to directive '${
+      directive.name
+    }'`,
+    path: `@${directive.name}`,
+  };
+}
+
+export function directiveArgumentRemoved(
+  directive: GraphQLDirective,
+  arg: GraphQLArgument,
+): Change {
+  return {
+    criticality: {
+      level: CriticalityLevel.Breaking,
+    },
+    message: `Argument '${arg.name}' was removed from directive '${
+      directive.name
+    }'`,
+    path: `@${directive.name}.${arg.name}`,
+  };
+}
+
+export function directiveArgumentDescriptionChanged(
+  directive: GraphQLDirective,
+  oldArg: GraphQLArgument,
+  newArg: GraphQLArgument,
+): Change {
+  return {
+    criticality: {
+      level: CriticalityLevel.NonBreaking,
+    },
+    message: `Description for argument '${oldArg.name}' on directive '${
+      directive.name
+    }' changed from '${oldArg.description}' to '${newArg.description}'`,
+    path: `@${directive.name}.${oldArg.name}`,
+  };
+}
+
+export function directiveArgumentDefaultValueChanged(
+  directive: GraphQLDirective,
+  oldArg: GraphQLArgument,
+  newArg: GraphQLArgument,
+): Change {
+  return {
+    criticality: {
+      level: CriticalityLevel.Dangerous,
+      reason:
+        'Changing the default value for an argument may change the runtime behaviour of a field if it was never provided.',
+    },
+    message: `Default value for argument '${oldArg.name}' on directive '${
+      directive.name
+    }' changed from '${oldArg.defaultValue}' to '${newArg.defaultValue}'`,
+    path: `@${directive.name}.${oldArg.name}`,
+  };
+}
+
+export function directiveArgumentTypeChanged(
+  directive: GraphQLDirective,
+  oldArg: GraphQLArgument,
+  newArg: GraphQLArgument,
+): Change {
+  return {
+    criticality: safeChangeForInputValue(oldArg.type, newArg.type)
+      ? {
+          level: CriticalityLevel.NonBreaking,
+          reason:
+            'Changing an input field from non-null to null is considered non-breaking.',
+        }
+      : {
+          level: CriticalityLevel.Breaking,
+        },
+    message: `Type for argument '${oldArg.name}' on directive '${
+      directive.name
+    }' changed from '${oldArg.type}' to '${newArg.type}'`,
+    path: `@${directive.name}.${oldArg.name}`,
   };
 }
---
+++ src/diff/directive.ts
@@ -0,0 +1,105 @@
+import {
+  GraphQLDirective,
+  DirectiveLocationEnum,
+  GraphQLArgument,
+} from 'graphql';
+
+import {Change} from '../changes/change';
+import {
+  directiveDescriptionChanged,
+  directiveLocationAdded,
+  directiveLocationRemoved,
+  directiveArgumentAdded,
+  directiveArgumentRemoved,
+  directiveArgumentDescriptionChanged,
+  directiveArgumentDefaultValueChanged,
+  directiveArgumentTypeChanged,
+} from '../changes/directive';
+import {diffArrays, unionArrays} from '../utils/arrays';
+
+export function changesInEnum(
+  oldDirective: GraphQLDirective,
+  newDirective: GraphQLDirective,
+): Change[] {
+  const changes: Change[] = [];
+
+  if (oldDirective.description !== newDirective.description) {
+    changes.push(directiveDescriptionChanged(oldDirective, newDirective));
+  }
+
+  // locations added
+  changes.push(
+    ...diffArrays(newDirective.locations, oldDirective.locations).map(
+      location =>
+        directiveLocationAdded(newDirective, location as DirectiveLocationEnum),
+    ),
+  );
+
+  // locations removed
+  changes.push(
+    ...diffArrays(oldDirective.locations, newDirective.locations).map(
+      location =>
+        directiveLocationRemoved(
+          oldDirective,
+          location as DirectiveLocationEnum,
+        ),
+    ),
+  );
+
+  const oldNames = oldDirective.args.map(a => a.name);
+  const newNames = newDirective.args.map(a => a.name);
+
+  // arguments added
+  changes.push(
+    ...diffArrays(newNames, oldNames).map(name =>
+      directiveArgumentAdded(newDirective, newDirective.args.find(
+        a => a.name === name,
+      ) as GraphQLArgument),
+    ),
+  );
+
+  // arguments removed
+  changes.push(
+    ...diffArrays(oldNames, newNames).map(name =>
+      directiveArgumentRemoved(newDirective, newDirective.args.find(
+        a => a.name === name,
+      ) as GraphQLArgument),
+    ),
+  );
+
+  // common arguments
+  unionArrays(oldNames, newNames).forEach(name => {
+    const oldArg = oldDirective.args.find(a => a.name === name);
+    const newArg = newDirective.args.find(a => a.name === name);
+
+    changes.push(...changesInDirectiveArgument(oldDirective, oldArg, newArg));
+  });
+
+  return changes;
+}
+
+function changesInDirectiveArgument(
+  directive: GraphQLDirective,
+  oldArg: GraphQLArgument,
+  newArg: GraphQLArgument,
+): Change[] {
+  const changes: Change[] = [];
+
+  if (oldArg.description !== newArg.description) {
+    changes.push(
+      directiveArgumentDescriptionChanged(directive, oldArg, newArg),
+    );
+  }
+
+  if (oldArg.defaultValue !== newArg.defaultValue) {
+    changes.push(
+      directiveArgumentDefaultValueChanged(directive, oldArg, newArg),
+    );
+  }
+
+  if (oldArg.type.toString() !== newArg.type.toString()) {
+    changes.push(directiveArgumentTypeChanged(directive, oldArg, newArg));
+  }
+
+  return changes;
+}
