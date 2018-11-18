9763a59f17e058cd5f6f36b2b3f5592ddd43f20c
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-11-18 22:07:07 +0100

    Serve command

---
+++ src/cli/commands/serve.ts
@@ -0,0 +1,39 @@
+import * as logSymbols from 'log-symbols';
+import chalk from 'chalk';
+import {ApolloServer} from 'apollo-server';
+import opn = require('opn');
+
+import {loadSchema} from '../loaders/schema';
+import {Renderer, ConsoleRenderer} from '../render';
+
+export async function serve(
+  schemaPointer: string,
+  options?: {
+    renderer?: Renderer;
+  },
+) {
+  const renderer = (options && options.renderer) || new ConsoleRenderer();
+  const schema = await loadSchema(schemaPointer);
+  const PORT = process.env.PORT || '4000';
+  const app = new ApolloServer({
+    cors: true,
+    mocks: true,
+    playground: true,
+    schema,
+  });
+
+  try {
+    const {url} = await app.listen(PORT);
+
+    renderer.emit(
+      chalk.greenBright(
+        logSymbols.success,
+        `Serving the GraphQL Playground on ${url}`,
+      ),
+    );
+
+    await opn(url);
+  } catch (e) {
+    renderer.emit(chalk.redBright(logSymbols.error, e.message || e));
+  }
+}
