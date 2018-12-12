67c55c4eb34d146b6af4777232b87dd1bb033405
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-12-12 10:51:10 +0100

    Extract documents from gql literal tags

--- packages/load/src/documents/from-glob-files.ts
+++ packages/load/src/documents/from-glob-files.ts
@@ -1,13 +1,22 @@
-import {Source} from 'graphql';
+import {Source, parse} from 'graphql';
 import {existsSync, readFileSync} from 'fs';
 import {extname} from 'path';
 import * as isGlob from 'is-glob';
 import * as glob from 'glob';
 import * as isValidPath from 'is-valid-path';
+import gqlPluck from 'graphql-tag-pluck';
 
 import {DocumentsHandler} from './loader';
 
-const graphQLExtensions = ['.gql', '.graphql', '.graphqls'];
+const extensions = {
+  graphql: ['.gql', '.graphql', '.graphqls'],
+  js: ['.js', '.jsx'],
+  ts: ['.ts', '.tsx'],
+};
+const flattenedExtensions = Object.values(extensions).reduce(
+  (acc, exts) => acc.concat(exts.map(ext => ext.substr(1))),
+  [],
+);
 
 async function documentsFromGlob(documentGlob: string): Promise<string[]> {
   return new Promise<string[]>((resolve, reject) =>
@@ -15,17 +24,40 @@ async function documentsFromGlob(documentGlob: string): Promise<string[]> {
   );
 }
 
+export function extractDocument(source: Source): string | void {
+  try {
+    const parsed = parse(source.body);
+
+    if (parsed) {
+      return source.body;
+    }
+  } catch (e) {
+    return gqlPluck.fromFile.sync(source.name);
+  }
+}
+
 async function readFile(filepath: string): Promise<Source> {
   if (existsSync(filepath)) {
     const fileContent = readFileSync(filepath, 'utf8');
-    const fileExt = extname(filepath);
+    const fileExt = extname(filepath).toLowerCase();
 
-    if (graphQLExtensions.indexOf(fileExt) !== -1) {
+    if (extensions.graphql.indexOf(fileExt) !== -1) {
       return new Source(fileContent, filepath);
     }
 
+    if (
+      extensions.ts.indexOf(fileExt) !== -1 ||
+      extensions.js.indexOf(fileExt) !== -1
+    ) {
+      const doc = extractDocument(new Source(fileContent, filepath));
+
+      if (doc) {
+        return new Source(doc, filepath);
+      }
+    }
+
     throw new Error(
-      `Only files with extensions: ${graphQLExtensions.join(
+      `Only files with extensions: ${flattenedExtensions.join(
         ', ',
       )} are allowed. We couldn't handle ${filepath}`,
     );
