91d0d5a19a34b03749dd612708860323ccc042b0
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-01-06 22:00:29 +0100

    Preprend

--- functions/webhook.js
+++ functions/webhook.js
@@ -1,6 +1,8 @@
 const {serverless} = require('@probot/serverless-lambda');
 const appFn = require('@graphql-inspector/github').default;
 
+console.log(process.env);
+
 exports.handler = serverless(app => {
   console.log(process.env);
   return appFn(app);
