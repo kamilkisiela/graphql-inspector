ce50bf6c4687064d53eca46e8b86a2427f025360
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-03-25 23:53:17 +0100

    An attempt to fix Github Action (#103)

--- packages/github-actions/src/index.ts
+++ packages/github-actions/src/index.ts
@@ -1,55 +1,54 @@
 #!/usr/bin/env node
 
-import appFn, {handleAction} from '@graphql-inspector/github';
+import {handleAction} from '@graphql-inspector/github';
 import * as uuid from 'uuid';
 import {resolve} from 'path';
-import {findPrivateKey} from 'probot/lib/private-key';
-import {createProbot, Application, ApplicationFunction} from 'probot';
+import {createProbot, Application} from 'probot';
 
 const event = process.env.GITHUB_EVENT_NAME;
-const cert = findPrivateKey()!;
 const payload = require(resolve(process.env.GITHUB_EVENT_PATH!));
+const githubToken = process.env.GITHUB_TOKEN;
 
 const probot = createProbot({
-  id: parseInt(process.env.APP_ID!, 10),
-  cert: cert as string,
-  githubToken: process.env.GITHUB_TOKEN,
+  githubToken,
 });
 
-function wrapAppFn(fn: ApplicationFunction) {
-  return (app: Application) => {
-    fn(app);
+probot.logger.info(`Running probot`);
 
-    app.on('pull_request', async context => {
-      if (!process.env.GITHUB_ACTION) {
-        return;
-      }
-      const ref = context.payload.pull_request.head.sha;
-      const action = 'gh-action';
-      const {owner, repo} = context.repo();
+function appFn(app: Application) {
+  app.on('pull_request', async context => {
+    if (!process.env.GITHUB_ACTION) {
+      context.log.error('No GITHUB_ACTION available!');
+      return;
+    }
+    const ref = context.payload.pull_request.head.sha;
+    const action = 'gh-action';
+    const {owner, repo} = context.repo();
 
-      await handleAction({context, owner, repo, ref, action});
-      process.exit(0);
-    });
+    await handleAction({context, owner, repo, ref, action});
+    process.exit(0);
+  });
 
-    app.on('push', async context => {
-      if (!process.env.GITHUB_ACTION) {
-        return;
-      }
+  app.on('push', async context => {
+    if (!process.env.GITHUB_ACTION) {
+      context.log.error('No GITHUB_ACTION available!');
+      return;
+    }
 
-      const ref = context.payload.after;
-      const action = 'gh-action';
-      const {owner, repo} = context.repo();
+    const ref = context.payload.after;
+    const action = 'gh-action';
+    const {owner, repo} = context.repo();
 
-      await handleAction({context, owner, repo, ref, action});
-      process.exit(0);
-    });
-  };
+    await handleAction({context, owner, repo, ref, action});
+    process.exit(0);
+  });
 }
 
-probot.setup([wrapAppFn(appFn)]);
+probot.logger.info(`Setting up probot`);
+probot.setup([appFn]);
 
-probot.receive({name: event!, payload, id: uuid.v4()}).catch(() => {
+probot.receive({name: event!, payload, id: uuid.v4()}).catch(error => {
+  probot.logger.info(error);
   // Process must exist non-zero to indicate that the action failed to run
   process.exit(1);
 });
