88e66376495b43706fc0ec3dde5823c4a6ea1d94
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-03-29 23:28:58 +0100

    Once for all

--- actions/cleanup/index.js
+++ actions/cleanup/index.js
@@ -3,15 +3,18 @@ const {Toolkit} = require('actions-toolkit');
 
 Toolkit.run(
   async tools => {
-    tools.log.info(tools.context.payload.pull_request);
+    const payload = tools.context.payload;
+
     tools.log.info('Cleaning up after Pull Request was merged or closed');
+
     await tools.github.git.deleteRef(
       tools.context.repo({
-        ref: `heads/${tools.context.payload.pull_request.head.ref}`,
+        ref: `heads/${payload.pull_request.head.ref}`,
       }),
     );
+
     return tools.log.success(
-      `Branch ${tools.context.payload.pull_request.head.ref} deleted!`,
+      `Branch ${payload.pull_request.head.ref} deleted!`,
     );
   },
   {
