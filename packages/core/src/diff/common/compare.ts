98d64a5c6c6525ddf466483b53de0bf44a37fece
Jordan Gensler <jgensler@netflix.com>
2019-04-01 16:23:43 -0700

    Add array comparison to diffing

--- packages/core/src/diff/common/compare.ts
+++ packages/core/src/diff/common/compare.ts
@@ -1,4 +1,16 @@
 export function isEqual<T>(a: T, b: T): boolean {
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length) return false;
+
+    for (var index = 0; index < a.length; index++) {
+      if (a[index] !== b[index]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   return a === b || (!a && !b);
 }
 
