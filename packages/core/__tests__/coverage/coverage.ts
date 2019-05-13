fb1d2f496d89b6923ea9159d35047c575f80cf3f
Alex Ostapenko <alex.alx13@gmail.com>
2019-05-13 23:11:18 +0200

    Resolve #266
    Initiate coverage object for Interface types

--- packages/core/__tests__/coverage/coverage.ts
+++ packages/core/__tests__/coverage/coverage.ts
@@ -6,16 +6,22 @@ import {coverage} from '../../src/index';
 describe('coverage', () => {
   test('basic', () => {
     const a = buildSchema(/* GraphQL */ `
-      type Post {
+      interface Identifiable {
         id: ID
+        createdAt: Int
+      }
+
+      type Post implements Identifiable {
+        id: ID
+        createdAt: Int
         title: String
         author: String
-        createdAt: Int
       }
 
       type Query {
         post: Post
         posts: [Post!]
+        objectById(id: ID!): Identifiable
       }
 
       schema {
@@ -30,19 +36,32 @@ describe('coverage', () => {
           title
         }
       }
+      query getObjectById {
+        objectById {
+          id
+          ... on Post {
+            title
+            createdAt
+          }
+        }
+      }
     `;
 
     const results = coverage(a, [new Source(print(doc))]);
 
     // Query
-    expect(results.types.Query.hits).toEqual(1);
+    expect(results.types.Query.hits).toEqual(2);
     expect(results.types.Query.children.posts.hits).toEqual(0);
     expect(results.types.Query.children.post.hits).toEqual(1);
     // Post
-    expect(results.types.Post.hits).toEqual(2);
+    expect(results.types.Post.hits).toEqual(4);
     expect(results.types.Post.children.id.hits).toEqual(1);
-    expect(results.types.Post.children.title.hits).toEqual(1);
+    expect(results.types.Post.children.title.hits).toEqual(2);
     expect(results.types.Post.children.author.hits).toEqual(0);
-    expect(results.types.Post.children.createdAt.hits).toEqual(0);
+    expect(results.types.Post.children.createdAt.hits).toEqual(1);
+    // Identifiable
+    expect(results.types.Identifiable.hits).toEqual(1);
+    expect(results.types.Identifiable.children.id.hits).toEqual(1);
+    expect(results.types.Identifiable.children.createdAt.hits).toEqual(0);
   });
 });
