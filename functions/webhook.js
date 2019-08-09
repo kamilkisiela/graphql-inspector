1a7c6d26082976bb71f27d55e133c15028e9b16c
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-08-09 22:28:14 +0200

    Move to Zeit and Now (#513)

--- functions/webhook.js
+++
@@ -1,9 +0,0 @@
-const {serverless} = require('@probot/serverless-lambda');
-const appFn = require('@graphql-inspector/github').default;
-
-console.log(process.env);
-
-exports.handler = serverless(app => {
-  console.log(process.env);
-  return appFn(app);
-});
