e6758ddc1fbd9212bac2f93617ef6621d13973bd
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-09-26 21:20:49 +0200

    Update and use hash

--- website/live/src/App.js
+++ website/live/src/App.js
@@ -1,21 +1,17 @@
 import React, {lazy, Suspense} from 'react';
-import {useRouter} from 'react-tiniest-router';
 import Loading from './Loading';
-import routes from './routes';
 
 const Diff = lazy(() => import('./diff/Diff'));
 const Contact = lazy(() => import('./contact/Contact'));
 
 function App() {
-  const {isRoute} = useRouter();
-
-  console.log('got to App');
+  const page = window.location.hash.replace('#', '');
+  const isContact = page === 'contact';
 
   return (
     <Suspense fallback={<Loading />}>
-      working!
-      {isRoute(routes.diff) && <Diff />}
-      {isRoute(routes.contact) && <Contact />}
+      {isContact && <Contact />}
+      {!isContact && <Diff />}
     </Suspense>
   );
 }
