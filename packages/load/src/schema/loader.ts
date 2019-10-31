893ded1ff1455cbd35c18c680660596fd8a5c1d7
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-10-31 13:31:11 +0100

    Use new loaders packages from graphql-toolkit

--- packages/load/src/schema/loader.ts
+++
@@ -1,7 +0,0 @@
-import {GraphQLSchema} from 'graphql';
-
-export type SchemaLoader = () => Promise<GraphQLSchema>;
-export type SchemaHandler = (
-  pointer: string,
-  extra?: any,
-) => void | (SchemaLoader);
