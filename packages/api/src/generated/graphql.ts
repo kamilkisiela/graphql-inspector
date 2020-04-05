f1f48c8c09576b48531e8be07b33455e29460b73
Dotan Simha <dotansimha@gmail.com>
2020-04-05 09:44:21 +0300

    workaround issue with typings error

--- packages/api/src/generated/graphql.ts
+++ packages/api/src/generated/graphql.ts
@@ -13,81 +13,6 @@ export type Scalars = {
   Float: number;
 };
 
-export type Change = {
-  readonly __typename?: 'Change';
-  readonly message: Scalars['String'];
-  readonly path?: Maybe<Scalars['String']>;
-  readonly type: Scalars['String'];
-  readonly criticality: Criticality;
-};
-
-export type Criticality = {
-  readonly __typename?: 'Criticality';
-  readonly level: CriticalityLevel;
-  readonly reason?: Maybe<Scalars['String']>;
-};
-
-export enum CriticalityLevel {
-  Breaking = 'BREAKING',
-  NonBreaking = 'NON_BREAKING',
-  Dangerous = 'DANGEROUS'
-}
-
-export type DocumentLocation = {
-  readonly __typename?: 'DocumentLocation';
-  readonly name: Scalars['String'];
-  readonly locations?: Maybe<ReadonlyArray<Location>>;
-};
-
-export type DocumentSource = {
-  readonly __typename?: 'DocumentSource';
-  readonly body: Scalars['String'];
-  readonly name: Scalars['String'];
-};
-
-export type GraphQlError = {
-  readonly __typename?: 'GraphQLError';
-  /** A message describing the Error for debugging purposes. */
-  readonly message: Scalars['String'];
-  /**
-   * An array of { line, column } locations within the source GraphQL document
-   * which correspond to this error.
-   * 
-   * Errors during validation often contain multiple locations, for example to
-   * point out two things with the same name. Errors during execution include a
-   * single location, the field which produced the error.
-   */
-  readonly locations?: Maybe<ReadonlyArray<SourceLocation>>;
-  /**
-   * An array of character offsets within the source GraphQL document
-   * which correspond to this error.
-   */
-  readonly positions?: Maybe<ReadonlyArray<Maybe<Scalars['Int']>>>;
-};
-
-export type InvalidDocument = {
-  readonly __typename?: 'InvalidDocument';
-  readonly source: DocumentSource;
-  readonly errors?: Maybe<ReadonlyArray<GraphQlError>>;
-};
-
-export type Location = {
-  readonly __typename?: 'Location';
-  readonly start: Scalars['Int'];
-  readonly end: Scalars['Int'];
-};
-
-export type Match = {
-  readonly __typename?: 'Match';
-  readonly name: Scalars['String'];
-  readonly rating: Scalars['Float'];
-};
-
-export type Mutation = {
-  readonly __typename?: 'Mutation';
-  readonly ping: Scalars['String'];
-};
-
 export type Query = {
   readonly __typename?: 'Query';
   readonly ping: Scalars['String'];
@@ -129,23 +54,53 @@ export type QuerySimilarToArgs = {
   threshold?: Maybe<Scalars['Float']>;
 };
 
-export type SchemaCoverage = {
-  readonly __typename?: 'SchemaCoverage';
-  readonly sources?: Maybe<ReadonlyArray<DocumentSource>>;
-  readonly types?: Maybe<ReadonlyArray<TypeCoverage>>;
+export type Mutation = {
+  readonly __typename?: 'Mutation';
+  readonly ping: Scalars['String'];
 };
 
-export type Similar = {
-  readonly __typename?: 'Similar';
+export type DocumentSource = {
+  readonly __typename?: 'DocumentSource';
+  readonly body: Scalars['String'];
   readonly name: Scalars['String'];
-  readonly best: Match;
-  readonly types?: Maybe<ReadonlyArray<Match>>;
 };
 
 export type SourceLocation = {
   readonly __typename?: 'SourceLocation';
   readonly line: Scalars['Int'];
   readonly column: Scalars['Int'];
+};
+
+export type GraphQlError = {
+  readonly __typename?: 'GraphQLError';
+  /** A message describing the Error for debugging purposes. */
+  readonly message: Scalars['String'];
+  /**
+   * An array of { line, column } locations within the source GraphQL document
+   * which correspond to this error.
+   * 
+   * Errors during validation often contain multiple locations, for example to
+   * point out two things with the same name. Errors during execution include a
+   * single location, the field which produced the error.
+   */
+  readonly locations?: Maybe<ReadonlyArray<SourceLocation>>;
+  /**
+   * An array of character offsets within the source GraphQL document
+   * which correspond to this error.
+   */
+  readonly positions?: Maybe<ReadonlyArray<Maybe<Scalars['Int']>>>;
+};
+
+export type Location = {
+  readonly __typename?: 'Location';
+  readonly start: Scalars['Int'];
+  readonly end: Scalars['Int'];
+};
+
+export type DocumentLocation = {
+  readonly __typename?: 'DocumentLocation';
+  readonly name: Scalars['String'];
+  readonly locations?: Maybe<ReadonlyArray<Location>>;
 };
 
 export type TypeChildCoverage = {
@@ -160,6 +115,51 @@ export type TypeCoverage = {
   readonly name: Scalars['String'];
   readonly hits: Scalars['Int'];
   readonly children?: Maybe<ReadonlyArray<TypeChildCoverage>>;
+};
+
+export type SchemaCoverage = {
+  readonly __typename?: 'SchemaCoverage';
+  readonly sources?: Maybe<ReadonlyArray<DocumentSource>>;
+  readonly types?: Maybe<ReadonlyArray<TypeCoverage>>;
+};
+
+export enum CriticalityLevel {
+  Breaking = 'BREAKING',
+  NonBreaking = 'NON_BREAKING',
+  Dangerous = 'DANGEROUS'
+}
+
+export type Criticality = {
+  readonly __typename?: 'Criticality';
+  readonly level: CriticalityLevel;
+  readonly reason?: Maybe<Scalars['String']>;
+};
+
+export type Change = {
+  readonly __typename?: 'Change';
+  readonly message: Scalars['String'];
+  readonly path?: Maybe<Scalars['String']>;
+  readonly type: Scalars['String'];
+  readonly criticality: Criticality;
+};
+
+export type InvalidDocument = {
+  readonly __typename?: 'InvalidDocument';
+  readonly source: DocumentSource;
+  readonly errors?: Maybe<ReadonlyArray<GraphQlError>>;
+};
+
+export type Match = {
+  readonly __typename?: 'Match';
+  readonly name: Scalars['String'];
+  readonly rating: Scalars['Float'];
+};
+
+export type Similar = {
+  readonly __typename?: 'Similar';
+  readonly name: Scalars['String'];
+  readonly best: Match;
+  readonly types?: Maybe<ReadonlyArray<Match>>;
 };
 
 
@@ -235,105 +235,50 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
-  Query: ResolverTypeWrapper<{}>,
   String: ResolverTypeWrapper<Scalars['String']>,
-  SchemaCoverage: ResolverTypeWrapper<core.SchemaCoverage>,
-  DocumentSource: ResolverTypeWrapper<DocumentSource>,
-  TypeCoverage: ResolverTypeWrapper<ResolvedTypeCoverage>,
-  Int: ResolverTypeWrapper<Scalars['Int']>,
-  TypeChildCoverage: ResolverTypeWrapper<ResolvedTypeChildCoverage>,
-  DocumentLocation: ResolverTypeWrapper<ResolvedDocumentLocation>,
-  Location: ResolverTypeWrapper<Location>,
-  Change: ResolverTypeWrapper<Change>,
-  Criticality: ResolverTypeWrapper<Criticality>,
-  CriticalityLevel: CriticalityLevel,
-  InvalidDocument: ResolverTypeWrapper<InvalidDocument>,
-  GraphQLError: ResolverTypeWrapper<GraphQlError>,
-  SourceLocation: ResolverTypeWrapper<SourceLocation>,
-  Float: ResolverTypeWrapper<Scalars['Float']>,
-  Similar: ResolverTypeWrapper<ResolvedSimilar>,
-  Match: ResolverTypeWrapper<Match>,
-  Mutation: ResolverTypeWrapper<{}>,
   Boolean: ResolverTypeWrapper<Scalars['Boolean']>,
+  Query: ResolverTypeWrapper<{}>,
+  Float: ResolverTypeWrapper<Scalars['Float']>,
+  Mutation: ResolverTypeWrapper<{}>,
+  DocumentSource: ResolverTypeWrapper<DocumentSource>,
+  SourceLocation: ResolverTypeWrapper<SourceLocation>,
+  Int: ResolverTypeWrapper<Scalars['Int']>,
+  GraphQLError: ResolverTypeWrapper<GraphQlError>,
+  Location: ResolverTypeWrapper<Location>,
+  DocumentLocation: ResolverTypeWrapper<ResolvedDocumentLocation>,
+  TypeChildCoverage: ResolverTypeWrapper<ResolvedTypeChildCoverage>,
+  TypeCoverage: ResolverTypeWrapper<ResolvedTypeCoverage>,
+  SchemaCoverage: ResolverTypeWrapper<core.SchemaCoverage>,
+  CriticalityLevel: CriticalityLevel,
+  Criticality: ResolverTypeWrapper<Criticality>,
+  Change: ResolverTypeWrapper<Change>,
+  InvalidDocument: ResolverTypeWrapper<InvalidDocument>,
+  Match: ResolverTypeWrapper<Match>,
+  Similar: ResolverTypeWrapper<ResolvedSimilar>,
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
-  Query: {},
   String: Scalars['String'],
-  SchemaCoverage: core.SchemaCoverage,
-  DocumentSource: DocumentSource,
-  TypeCoverage: ResolvedTypeCoverage,
-  Int: Scalars['Int'],
-  TypeChildCoverage: ResolvedTypeChildCoverage,
-  DocumentLocation: ResolvedDocumentLocation,
-  Location: Location,
-  Change: Change,
-  Criticality: Criticality,
-  CriticalityLevel: CriticalityLevel,
-  InvalidDocument: InvalidDocument,
-  GraphQLError: GraphQlError,
-  SourceLocation: SourceLocation,
-  Float: Scalars['Float'],
-  Similar: ResolvedSimilar,
-  Match: Match,
-  Mutation: {},
   Boolean: Scalars['Boolean'],
-};
-
-export type ChangeResolvers<ContextType = any, ParentType extends ResolversParentTypes['Change'] = ResolversParentTypes['Change']> = {
-  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
-  path?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>,
-  type?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
-  criticality?: Resolver<ResolversTypes['Criticality'], ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
-};
-
-export type CriticalityResolvers<ContextType = any, ParentType extends ResolversParentTypes['Criticality'] = ResolversParentTypes['Criticality']> = {
-  level?: Resolver<ResolversTypes['CriticalityLevel'], ParentType, ContextType>,
-  reason?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
-};
-
-export type DocumentLocationResolvers<ContextType = any, ParentType extends ResolversParentTypes['DocumentLocation'] = ResolversParentTypes['DocumentLocation']> = {
-  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
-  locations?: Resolver<Maybe<ReadonlyArray<ResolversTypes['Location']>>, ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
-};
-
-export type DocumentSourceResolvers<ContextType = any, ParentType extends ResolversParentTypes['DocumentSource'] = ResolversParentTypes['DocumentSource']> = {
-  body?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
-  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
-};
-
-export type GraphQlErrorResolvers<ContextType = any, ParentType extends ResolversParentTypes['GraphQLError'] = ResolversParentTypes['GraphQLError']> = {
-  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
-  locations?: Resolver<Maybe<ReadonlyArray<ResolversTypes['SourceLocation']>>, ParentType, ContextType>,
-  positions?: Resolver<Maybe<ReadonlyArray<Maybe<ResolversTypes['Int']>>>, ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
-};
-
-export type InvalidDocumentResolvers<ContextType = any, ParentType extends ResolversParentTypes['InvalidDocument'] = ResolversParentTypes['InvalidDocument']> = {
-  source?: Resolver<ResolversTypes['DocumentSource'], ParentType, ContextType>,
-  errors?: Resolver<Maybe<ReadonlyArray<ResolversTypes['GraphQLError']>>, ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
-};
-
-export type LocationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Location'] = ResolversParentTypes['Location']> = {
-  start?: Resolver<ResolversTypes['Int'], ParentType, ContextType>,
-  end?: Resolver<ResolversTypes['Int'], ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
-};
-
-export type MatchResolvers<ContextType = any, ParentType extends ResolversParentTypes['Match'] = ResolversParentTypes['Match']> = {
-  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
-  rating?: Resolver<ResolversTypes['Float'], ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
-};
-
-export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
-  ping?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  Query: {},
+  Float: Scalars['Float'],
+  Mutation: {},
+  DocumentSource: DocumentSource,
+  SourceLocation: SourceLocation,
+  Int: Scalars['Int'],
+  GraphQLError: GraphQlError,
+  Location: Location,
+  DocumentLocation: ResolvedDocumentLocation,
+  TypeChildCoverage: ResolvedTypeChildCoverage,
+  TypeCoverage: ResolvedTypeCoverage,
+  SchemaCoverage: core.SchemaCoverage,
+  CriticalityLevel: CriticalityLevel,
+  Criticality: Criticality,
+  Change: Change,
+  InvalidDocument: InvalidDocument,
+  Match: Match,
+  Similar: ResolvedSimilar,
 };
 
 export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
@@ -345,22 +290,38 @@ export type QueryResolvers<ContextType = any, ParentType extends ResolversParent
   similarTo?: Resolver<ResolversTypes['Similar'], ParentType, ContextType, RequireFields<QuerySimilarToArgs, 'schema' | 'name'>>,
 };
 
-export type SchemaCoverageResolvers<ContextType = any, ParentType extends ResolversParentTypes['SchemaCoverage'] = ResolversParentTypes['SchemaCoverage']> = {
-  sources?: Resolver<Maybe<ReadonlyArray<ResolversTypes['DocumentSource']>>, ParentType, ContextType>,
-  types?: Resolver<Maybe<ReadonlyArray<ResolversTypes['TypeCoverage']>>, ParentType, ContextType>,
-  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
+  ping?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
 };
 
-export type SimilarResolvers<ContextType = any, ParentType extends ResolversParentTypes['Similar'] = ResolversParentTypes['Similar']> = {
+export type DocumentSourceResolvers<ContextType = any, ParentType extends ResolversParentTypes['DocumentSource'] = ResolversParentTypes['DocumentSource']> = {
+  body?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
   name?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
-  best?: Resolver<ResolversTypes['Match'], ParentType, ContextType>,
-  types?: Resolver<Maybe<ReadonlyArray<ResolversTypes['Match']>>, ParentType, ContextType>,
   __isTypeOf?: isTypeOfResolverFn<ParentType>,
 };
 
 export type SourceLocationResolvers<ContextType = any, ParentType extends ResolversParentTypes['SourceLocation'] = ResolversParentTypes['SourceLocation']> = {
   line?: Resolver<ResolversTypes['Int'], ParentType, ContextType>,
   column?: Resolver<ResolversTypes['Int'], ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+};
+
+export type GraphQlErrorResolvers<ContextType = any, ParentType extends ResolversParentTypes['GraphQLError'] = ResolversParentTypes['GraphQLError']> = {
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  locations?: Resolver<Maybe<ReadonlyArray<ResolversTypes['SourceLocation']>>, ParentType, ContextType>,
+  positions?: Resolver<Maybe<ReadonlyArray<Maybe<ResolversTypes['Int']>>>, ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+};
+
+export type LocationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Location'] = ResolversParentTypes['Location']> = {
+  start?: Resolver<ResolversTypes['Int'], ParentType, ContextType>,
+  end?: Resolver<ResolversTypes['Int'], ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+};
+
+export type DocumentLocationResolvers<ContextType = any, ParentType extends ResolversParentTypes['DocumentLocation'] = ResolversParentTypes['DocumentLocation']> = {
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  locations?: Resolver<Maybe<ReadonlyArray<ResolversTypes['Location']>>, ParentType, ContextType>,
   __isTypeOf?: isTypeOfResolverFn<ParentType>,
 };
 
@@ -378,22 +339,61 @@ export type TypeCoverageResolvers<ContextType = any, ParentType extends Resolver
   __isTypeOf?: isTypeOfResolverFn<ParentType>,
 };
 
+export type SchemaCoverageResolvers<ContextType = any, ParentType extends ResolversParentTypes['SchemaCoverage'] = ResolversParentTypes['SchemaCoverage']> = {
+  sources?: Resolver<Maybe<ReadonlyArray<ResolversTypes['DocumentSource']>>, ParentType, ContextType>,
+  types?: Resolver<Maybe<ReadonlyArray<ResolversTypes['TypeCoverage']>>, ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+};
+
+export type CriticalityResolvers<ContextType = any, ParentType extends ResolversParentTypes['Criticality'] = ResolversParentTypes['Criticality']> = {
+  level?: Resolver<ResolversTypes['CriticalityLevel'], ParentType, ContextType>,
+  reason?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+};
+
+export type ChangeResolvers<ContextType = any, ParentType extends ResolversParentTypes['Change'] = ResolversParentTypes['Change']> = {
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  path?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>,
+  type?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  criticality?: Resolver<ResolversTypes['Criticality'], ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+};
+
+export type InvalidDocumentResolvers<ContextType = any, ParentType extends ResolversParentTypes['InvalidDocument'] = ResolversParentTypes['InvalidDocument']> = {
+  source?: Resolver<ResolversTypes['DocumentSource'], ParentType, ContextType>,
+  errors?: Resolver<Maybe<ReadonlyArray<ResolversTypes['GraphQLError']>>, ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+};
+
+export type MatchResolvers<ContextType = any, ParentType extends ResolversParentTypes['Match'] = ResolversParentTypes['Match']> = {
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  rating?: Resolver<ResolversTypes['Float'], ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+};
+
+export type SimilarResolvers<ContextType = any, ParentType extends ResolversParentTypes['Similar'] = ResolversParentTypes['Similar']> = {
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  best?: Resolver<ResolversTypes['Match'], ParentType, ContextType>,
+  types?: Resolver<Maybe<ReadonlyArray<ResolversTypes['Match']>>, ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+};
+
 export type Resolvers<ContextType = any> = {
-  Change?: ChangeResolvers<ContextType>,
-  Criticality?: CriticalityResolvers<ContextType>,
-  DocumentLocation?: DocumentLocationResolvers<ContextType>,
-  DocumentSource?: DocumentSourceResolvers<ContextType>,
-  GraphQLError?: GraphQlErrorResolvers<ContextType>,
-  InvalidDocument?: InvalidDocumentResolvers<ContextType>,
-  Location?: LocationResolvers<ContextType>,
-  Match?: MatchResolvers<ContextType>,
-  Mutation?: MutationResolvers<ContextType>,
   Query?: QueryResolvers<ContextType>,
-  SchemaCoverage?: SchemaCoverageResolvers<ContextType>,
-  Similar?: SimilarResolvers<ContextType>,
+  Mutation?: MutationResolvers<ContextType>,
+  DocumentSource?: DocumentSourceResolvers<ContextType>,
   SourceLocation?: SourceLocationResolvers<ContextType>,
+  GraphQLError?: GraphQlErrorResolvers<ContextType>,
+  Location?: LocationResolvers<ContextType>,
+  DocumentLocation?: DocumentLocationResolvers<ContextType>,
   TypeChildCoverage?: TypeChildCoverageResolvers<ContextType>,
   TypeCoverage?: TypeCoverageResolvers<ContextType>,
+  SchemaCoverage?: SchemaCoverageResolvers<ContextType>,
+  Criticality?: CriticalityResolvers<ContextType>,
+  Change?: ChangeResolvers<ContextType>,
+  InvalidDocument?: InvalidDocumentResolvers<ContextType>,
+  Match?: MatchResolvers<ContextType>,
+  Similar?: SimilarResolvers<ContextType>,
 };
 
 
