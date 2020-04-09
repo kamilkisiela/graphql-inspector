7533ad09bc2ff32606f25d6eea3e4fee565bc013
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-04-09 13:52:58 +0200

    test: make sure we extendSchema

---
+++ packages/commands/introspect/__tests__/introspect.ts
@@ -0,0 +1,12 @@
+// import '@graphql-inspector/testing';
+// import {mockCommand} from '@graphql-inspector/commands';
+// import {mockLogger, unmockLogger} from '@graphql-inspector/logger';
+// import yargs from 'yargs';
+// import {buildSchema} from 'graphql';
+// import {resolve} from 'path';
+// import createCommand from '../src';
+
+
+describe('introspect', () => {
+  test.todo('make sure we extendSchema');
+});
