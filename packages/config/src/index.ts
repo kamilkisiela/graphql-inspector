194a3510ec6683d8c900df77432244032b3fbae8
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-04-09 11:15:16 +0200

    ci: auto discovery of commands and loaders

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,6 +1,3 @@
-import {cosmiconfig} from 'cosmiconfig';
-import {isAbsolute, resolve} from 'path';
-
 export interface InspectorConfig {
   use: {
     loaders: string[];
@@ -8,39 +5,53 @@ export interface InspectorConfig {
   };
 }
 
-interface UseConfigOptions {
-  config?: string;
-}
-
-export async function useConfig(
-  options?: UseConfigOptions,
-): Promise<InspectorConfig | never> {
-  const cosmi = cosmiconfig('inspector', {
-    cache: true,
-  });
-
-  const config = await (options?.config
-    ? cosmi.load(
-        isAbsolute(options.config)
-          ? options.config
-          : resolve(process.cwd(), options.config),
-      )
-    : cosmi.search());
-
-  if (!config) {
-    throw new Error('Config not found');
-  }
-
-  if (config.isEmpty) {
-    throw new Error('Config is empty');
-  }
-
+export async function useConfig(): Promise<InspectorConfig | never> {
   return {
     use: {
-      loaders: ensureList(config.config?.use?.loaders, 'loaders'),
-      commands: ensureList(config.config?.use?.commands, 'commands'),
+      loaders: ensureList(
+        discoverLoaders(['code', 'git', 'github', 'graphql', 'json', 'url']),
+        'loaders',
+      ),
+      commands: ensureList(
+        discoverCommands([
+          'coverage',
+          'diff',
+          'docs',
+          'introspect',
+          'serve',
+          'similar',
+          'validate',
+        ]),
+        'commands',
+      ),
     },
   };
+}
+
+function moduleExists(name: string) {
+  try {
+    require(name);
+
+    return true;
+  } catch (error) {
+    if (error?.message?.includes(`Cannot find module '${name}'`)) {
+      return false;
+    }
+
+    throw error;
+  }
+}
+
+function discoverLoaders(loaders: string[]) {
+  const names = loaders.map((name) => `@graphql-inspector/${name}-loader`);
+
+  return names.filter(moduleExists);
+}
+
+function discoverCommands(commands: string[]) {
+  const names = commands.map((name) => `@graphql-inspector/${name}-command`);
+
+  return names.filter(moduleExists);
 }
 
 function ensureList<T>(list: any, path: string): T[] {
