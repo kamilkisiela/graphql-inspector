0b4b5eaf8e8aa79894d9e8cf8d71e8dbb57b81ba
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-04-16 07:17:22 +0200

    Update footer (#1426)
    * Update probot.ts
    
    * Update run.ts

--- packages/action/src/run.ts
+++ packages/action/src/run.ts
@@ -166,7 +166,7 @@ export async function run() {
     [
       '',
       '___',
-      `Thank you for using [GraphQL Inspector](https://graphql-inspector.com/)`,
+      `Thank you for using [GraphQL Inspector](https://graphql-inspector.com/). If you like it, [consider supporting the project](https://github.com/sponsors/kamilkisiela).`,
     ].join('\n'),
   );
 
