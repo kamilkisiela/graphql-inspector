ee7c18c5a36c74f68b9e27782dbef00b956dcbd1
Gil Gardosh <gilgardosh@gmail.com>
2020-11-16 18:11:13 +0200

    updated test

--- packages/core/__tests__/validate/aws.ts
+++ packages/core/__tests__/validate/aws.ts
@@ -1,34 +1,15 @@
-import {buildSchema, Source, print, parse} from 'graphql';
+import {
+  // buildSchema,
+  Source,
+  print,
+  parse,
+} from 'graphql';
+import {LoadersRegistry} from '@graphql-inspector/loaders';
 
 import {validate} from '../../src/validate';
 
 describe('aws', () => {
-  test('should accept AWS Appsync types', () => {
-    const schema = buildSchema(
-      /* GraphQL */ `
-        type AWS_Data {
-          normalScalar: String
-          date: AWSDate!
-          time: AWSTime!
-          dateTime: AWSDateTime!
-          timestamp: AWSTimestamp!
-          email: AWSEmail!
-          json: AWSJSON!
-          url: AWSURL!
-          phone: AWSPhone!
-          ipAddress: AWSIPAddress!
-        }
-
-        type Query {
-          data: AWS_Data
-        }
-      `,
-      {
-        assumeValid: true,
-        assumeValidSDL: true,
-      },
-    );
-
+  test('should accept AWS Appsync types', async () => {
     const doc = parse(/* GraphQL */ `
       query getPost {
         data {
@@ -46,8 +27,32 @@ describe('aws', () => {
       }
     `);
 
+    const schema = await new LoadersRegistry().loadSchema(
+      `
+    type AWS_Data {
+      normalScalar: String
+      date: AWSDate!
+      time: AWSTime!
+      dateTime: AWSDateTime!
+      timestamp: AWSTimestamp!
+      email: AWSEmail!
+      json: AWSJSON!
+      url: AWSURL!
+      phone: AWSPhone!
+      ipAddress: AWSIPAddress!
+    }
+
+    type Query {
+      data: AWS_Data
+    }
+  `,
+      {},
+      false,
+      true,
+    );
+
     const results = validate(schema, [new Source(print(doc))], {
-      aws: true,
+      // aws: true,
     });
 
     expect(results).toHaveLength(0);
