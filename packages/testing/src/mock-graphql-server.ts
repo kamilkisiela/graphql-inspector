6124b9da08ec90db35b8ffca2a72dc4fe5d8339e
Gil Gardosh <gilgardosh@gmail.com>
2020-11-18 13:15:24 +0200

    use GET on introspect

--- packages/testing/src/mock-graphql-server.ts
+++ packages/testing/src/mock-graphql-server.ts
@@ -5,14 +5,33 @@ export function mockGraphQLServer({
   schema,
   host,
   path,
+  useGet = false,
 }: {
   schema: GraphQLSchema;
   host: string;
   path: string;
+  useGet?: boolean;
 }) {
-  const scope = nock(host)
-    .post(path)
-    .reply(async (_, body: any) => {
+  const scope = nock(host);
+  if (useGet) {
+    scope
+      .get((path) => path.startsWith(path))
+      .reply(async (unformattedQuery, _: any) => {
+        const query = new URL(host + unformattedQuery).searchParams.get(
+          'query',
+        );
+        try {
+          const result = await execute({
+            schema,
+            document: parse(query || ''),
+          });
+          return [200, result];
+        } catch (error) {
+          return [500, error];
+        }
+      });
+  } else {
+    scope.post(path).reply(async (_, body: any) => {
       try {
         const result = await execute({
           schema,
@@ -25,6 +44,7 @@ export function mockGraphQLServer({
         return [500, error];
       }
     });
+  }
 
   return () => {
     scope.done();
