24d9ccc1f6847e10b16f57c87bc8d29bd7c43956
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-04-06 10:32:23 +0000

    Fix website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -184,7 +184,6 @@ module.exports = {
           customCss: require.resolve('./src/css/custom.css'),
         },
         sitemap: {
-          cacheTime: 600 * 1001, // 600 sec - cache purge period
           changefreq: 'weekly',
           priority: 0.5,
         },
