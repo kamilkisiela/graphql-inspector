acf8590d9fc4161746b9a5f930ec51e5596d1246
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-04-06 09:12:46 +0000

    Adds a file path to an error message

--- packages/loaders/loaders/src/index.ts
+++ packages/loaders/loaders/src/index.ts
@@ -38,71 +38,75 @@ export class LoadersRegistry {
     enableApolloFederation: boolean,
     enableAWS: boolean,
   ): Promise<GraphQLSchema> {
-    return loadSchema(pointer, {
-      loaders: this.loaders,
-      ...options,
-      ...(enableApolloFederation
-        ? {
-            schemas: [
-              buildSchema(/* GraphQL */ `
-                scalar _FieldSet
-                directive @external on FIELD_DEFINITION
-                directive @requires(fields: _FieldSet!) on FIELD_DEFINITION
-                directive @provides(fields: _FieldSet!) on FIELD_DEFINITION
-                directive @key(fields: _FieldSet!) on OBJECT | INTERFACE
-                directive @extends on OBJECT | INTERFACE
-              `),
-            ],
-          }
-        : {}),
-      ...(enableAWS
-        ? {
-            schemas: [
-              buildSchema(/* GraphQL */ `
-                scalar AWSDate
-                scalar AWSTime
-                scalar AWSDateTime
-                scalar AWSTimestamp
-                scalar AWSEmail
-                scalar AWSJSON
-                scalar AWSURL
-                scalar AWSPhone
-                scalar AWSIPAddress
-                scalar BigInt
-                scalar Double
+    return enrichError(
+      loadSchema(pointer, {
+        loaders: this.loaders,
+        ...options,
+        ...(enableApolloFederation
+          ? {
+              schemas: [
+                buildSchema(/* GraphQL */ `
+                  scalar _FieldSet
+                  directive @external on FIELD_DEFINITION
+                  directive @requires(fields: _FieldSet!) on FIELD_DEFINITION
+                  directive @provides(fields: _FieldSet!) on FIELD_DEFINITION
+                  directive @key(fields: _FieldSet!) on OBJECT | INTERFACE
+                  directive @extends on OBJECT | INTERFACE
+                `),
+              ],
+            }
+          : {}),
+        ...(enableAWS
+          ? {
+              schemas: [
+                buildSchema(/* GraphQL */ `
+                  scalar AWSDate
+                  scalar AWSTime
+                  scalar AWSDateTime
+                  scalar AWSTimestamp
+                  scalar AWSEmail
+                  scalar AWSJSON
+                  scalar AWSURL
+                  scalar AWSPhone
+                  scalar AWSIPAddress
+                  scalar BigInt
+                  scalar Double
 
-                directive @aws_subscribe(
-                  mutations: [String!]!
-                ) on FIELD_DEFINITION
+                  directive @aws_subscribe(
+                    mutations: [String!]!
+                  ) on FIELD_DEFINITION
 
-                directive @deprecated(
-                  reason: String
-                ) on FIELD_DEFINITION | INPUT_FIELD_DEFINITION | ENUM | ENUM_VALUE
+                  directive @deprecated(
+                    reason: String
+                  ) on FIELD_DEFINITION | INPUT_FIELD_DEFINITION | ENUM | ENUM_VALUE
 
-                directive @aws_auth(
-                  cognito_groups: [String!]!
-                ) on FIELD_DEFINITION
-                directive @aws_api_key on FIELD_DEFINITION | OBJECT
-                directive @aws_iam on FIELD_DEFINITION | OBJECT
-                directive @aws_oidc on FIELD_DEFINITION | OBJECT
-                directive @aws_cognito_user_pools(
-                  cognito_groups: [String!]
-                ) on FIELD_DEFINITION | OBJECT
-              `),
-            ],
-          }
-        : {}),
-    });
+                  directive @aws_auth(
+                    cognito_groups: [String!]!
+                  ) on FIELD_DEFINITION
+                  directive @aws_api_key on FIELD_DEFINITION | OBJECT
+                  directive @aws_iam on FIELD_DEFINITION | OBJECT
+                  directive @aws_oidc on FIELD_DEFINITION | OBJECT
+                  directive @aws_cognito_user_pools(
+                    cognito_groups: [String!]
+                  ) on FIELD_DEFINITION | OBJECT
+                `),
+              ],
+            }
+          : {}),
+      }),
+    );
   }
 
   loadDocuments(
     pointer: SchemaPointerSingle,
     options: Omit<LoadTypedefsOptions, 'loaders'> = {},
   ): Promise<Source[]> {
-    return loadDocuments(pointer, {
-      loaders: this.loaders,
-      ...options,
-    });
+    return enrichError(
+      loadDocuments(pointer, {
+        loaders: this.loaders,
+        ...options,
+      }),
+    );
   }
 }
 
@@ -120,4 +124,16 @@ function loadModule<T>(name: string): T {
   const mod = require(name);
 
   return mod.default ? mod.default : mod;
+}
+
+/**
+ * Adds `(source: <file-path>)` suffix to error message if source is available
+ */
+function enrichError<T>(looksPromising: Promise<T>): Promise<T> {
+  return looksPromising.catch((error) => {
+    if (error.source?.name) {
+      error.message = `${error.message} (source: ${error.source?.name})`;
+    }
+    return Promise.reject(error);
+  });
 }
