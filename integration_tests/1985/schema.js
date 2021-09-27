dc79378644743b8da2fa8e08f04d16a8a6baf00d
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-09-27 14:29:15 +0000

    Add two integration tests

---
+++ integration_tests/1985/schema.js
@@ -0,0 +1,3 @@
+import {buildSchema} from 'graphql';
+
+export const schema = buildSchema(`type Query { foo: String }`);
---
+++ integration_tests/1991/two-operations.js
@@ -0,0 +1,13 @@
+import {gql} from 'graphql-tag';
+
+gql(/* GraphQL */ `
+  query isAuthenticated {
+    isLoggedIn
+  }
+`);
+
+export const UserFrgmnt = gql(/* GraphQL */ `
+  fragment UserFrgmnt on User {
+    id
+  }
+  `);
