dac35723a474d3af08b932cb2e548fb701831bc6
Jeremy Giberson <jeremy.giberson@formidable.com>
2022-02-17 10:20:50 -0700

    use rest api fallback to load schema if truncated

--- packages/action/src/run.ts
+++ packages/action/src/run.ts
@@ -238,6 +238,8 @@ function fileLoader({
       repository(name: $repo, owner: $owner) {
         object(expression: $expression) {
           ... on Blob {
+            isTruncated
+            oid
             text
           }
         }
@@ -264,10 +266,29 @@ function fileLoader({
     core.info(`Query ${file.ref}:${file.path} from ${owner}/${repo}`);
 
     try {
+      if( result?.repository?.object?.oid && result?.repository?.object?.isTruncated) {
+        const oid = result?.repository?.object?.oid
+        const getBlobResponse = await octokit.git.getBlob({
+          owner,
+          repo,
+          file_sha: oid,
+        });
+
+        if(getBlobResponse?.data?.content) {
+          return Buffer.from(getBlobResponse?.data?.content, 'base64').toString('utf-8')
+        }
+
+        throw new Error('getBlobResponse.data.content is null');
+      }
+
       if (
         result?.repository?.object?.text
       ) {
-        return result.repository.object.text;
+        if(result?.repository?.object?.isTruncated === false) {
+          return result.repository.object.text;
+        }
+
+        throw new Error('result.repository.object.text is truncated and oid is null');
       }
 
       throw new Error('result.repository.object.text is null');
