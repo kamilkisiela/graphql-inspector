9ce6ee87dbd195af22d660002e6340f04b918ea5
TuvalSimha <tuval.simha@gmail.com>
2022-07-27 14:08:05 +0300

    bootstrap
    bootstrap fix
    
    fix bootstrap

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,158 +1,37 @@
-import { FC } from 'react';
 import Script from 'next/script';
-import { appWithTranslation } from 'next-i18next';
-import {
-  extendTheme,
-  theme as chakraTheme,
-  useColorMode,
-} from '@chakra-ui/react';
-import { mode } from '@chakra-ui/theme-tools';
-import {
-  AppSeoProps,
-  CombinedThemeProvider,
-  DocsPage,
-  handlePushRoute,
-  useGoogleAnalytics,
-} from '@guild-docs/client';
-import { FooterExtended, Header, Subheader } from '@theguild/components';
-import type { AppProps } from 'next/app';
-import 'remark-admonitions/styles/infima.css';
-import '../../public/style.css';
-
-const styles: typeof chakraTheme['styles'] = {
-  global: (props) => ({
-    body: {
-      bg: mode('white', 'gray.850')(props),
-    },
-  }),
-};
+import { AppProps } from 'next/app';
+import { FooterExtended, Header, ThemeProvider } from '@theguild/components';
+import { useGoogleAnalytics } from 'guild-docs';
+import 'guild-docs/style.css';
 
 const accentColor = '#1cc8ee';
 
-const theme = extendTheme({
-  colors: {
-    gray: {
-      50: '#fafafa',
-      100: '#f5f5f5',
-      200: '#e5e5e5',
-      300: '#d4d4d4',
-      400: '#a3a3a3',
-      500: '#737373',
-      600: '#525252',
-      700: '#404040',
-      800: '#262626',
-      850: '#1b1b1b',
-      900: '#171717',
-    },
-    accentColor,
-  },
-  fonts: {
-    heading: 'TGCFont, sans-serif',
-    body: 'TGCFont, sans-serif',
-  },
-  config: {
-    initialColorMode: 'light',
-    useSystemColorMode: false,
-  },
-  styles,
-});
-
-const serializedMdx = process.env.SERIALIZED_MDX_ROUTES;
-const mdxRoutes = { data: serializedMdx && JSON.parse(serializedMdx) };
-
-const AppContent: FC<AppProps> = (appProps) => {
-  const { Component, pageProps, router } = appProps;
-  const isDocs = router.asPath.startsWith('/docs');
-  const { colorMode } = useColorMode();
-  const logoSrc =
-    colorMode === 'light' ? 'subheader-logo.svg' : 'subheader-logo-w.svg';
-
-  const analytics = useGoogleAnalytics({ router, trackingId: 'G-HQTC95CPEL' });
-
+export default function App({ Component, pageProps, router }: AppProps) {
+  const googleAnalytics = useGoogleAnalytics({
+    router,
+    trackingId: 'G-JSN4LT7S7V',
+  });
+  // @ts-expect-error -- getLayout is custom function from nextra
+  const { getLayout = (page) => page } = Component;
   return (
-    <>
+    <ThemeProvider>
       <Script async src="https://the-guild.dev/static/crisp.js" />
-      <Script {...analytics.loadScriptProps} />
-      <Script {...analytics.configScriptProps} />
-      <Header accentColor={accentColor} activeLink="/open-source" themeSwitch />
-      <Subheader
-        activeLink={router.asPath}
-        product={{
-          title: 'GraphQL Inspector',
-          description: 'Bulletproof your GraphQL',
-          image: {
-            src: `/assets/${logoSrc}`,
-            alt: 'Docs',
-          },
-          onClick: (e) => handlePushRoute('/', e),
-        }}
-        links={[
-          {
-            children: 'Home',
-            title: 'Read about GraphQL Inspector',
-            href: '/',
-            onClick: (e) => handlePushRoute('/', e),
-          },
-          {
-            children: 'Enterprise',
-            title: 'Try GraphQL Hive',
-            href: 'https://graphql-hive.com',
-          },
-          {
-            children: 'Docs',
-            title: 'View examples',
-            href: '/docs/introduction',
-            onClick: (e) => handlePushRoute('/docs/introduction', e),
-          },
-        ]}
-        cta={{
-          children: 'Get Started',
-          title: 'Start using GraphQL Inspector',
-          href: 'https://github.com/kamilkisiela/graphql-inspector',
-          target: '_blank',
-          rel: 'noopener noreferrer',
-        }}
-      />
-      {isDocs ? (
-        <DocsPage
-          appProps={appProps}
-          accentColor={accentColor}
-          mdxRoutes={mdxRoutes}
-        />
-      ) : (
-        <Component {...pageProps} />
-      )}
+      <Header accentColor={accentColor} themeSwitch searchBarProps={{ version: 'v2' }} />
+      <Script {...googleAnalytics.loadScriptProps} />
+      <Script {...googleAnalytics.configScriptProps} />
+      {/* @ts-ignore */}
+      {getLayout(<Component {...pageProps} />)}
       <FooterExtended />
-    </>
+    </ThemeProvider>
   );
-};
+}
 
-const AppContentWrapper = appWithTranslation(function TranslatedApp(appProps) {
-  return <AppContent {...appProps} />;
-});
-
-const defaultSeo: AppSeoProps = {
-  title: 'GraphQL Inspector',
-  description: `Validate schemas and detect changes. Receive schema change notifications. Keep Operations and Fragments consistent.`,
-  logo: {
-    url: 'https://graphql-inspector.com/assets/subheader-logo.png',
-    width: 50,
-    height: 54,
-  },
-};
-
-const App: FC<AppProps> = (appProps) => {
-  return (
-    <CombinedThemeProvider
-      theme={theme}
-      accentColor={accentColor}
-      defaultSeo={defaultSeo}
-    >
-      <>
-        <AppContentWrapper {...appProps} />
-      </>
-    </CombinedThemeProvider>
-  );
-};
-
-export default App;
+// const defaultSeo = {
+//   title: 'GraphQL Config',
+//   description: 'GraphQL Config',
+//   logo: {
+//     url: 'https://graphql-config.com/assets/subheader-logo.png',
+//     width: 50,
+//     height: 54,
+//   },
+// };
---
+++ website/theme.config.tsx
@@ -0,0 +1,42 @@
+import { ConfigLogo } from '@theguild/components';
+// @ts-ignore
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const SITE_NAME = 'GraphQL Scalars';
+
+const config: DocsThemeConfig = {
+  titleSuffix: ` – ${SITE_NAME}`,
+  projectLink: 'https://github.com/urigo/graphql-scalars', // GitHub link in the navbar
+  docsRepositoryBase: 'https://github.com/Urigo/graphql-scalars/tree/master/website/src/pages', // base URL for the docs repository
+  nextLinks: true,
+  prevLinks: true,
+  search: false,
+  floatTOC: true,
+  darkMode: true,
+  footer: false,
+  footerEditLink: 'Edit this page on GitHub',
+  logo: (
+    <>
+      <ConfigLogo className="mr-1.5 h-9 w-9" />
+      <div>
+        <h1 className="md:text-md text-sm font-medium">{SITE_NAME}</h1>
+        <h2 className="hidden text-xs sm:!block">
+          Common custom GraphQL Scalars for precise type-safe GraphQL schemas
+        </h2>
+      </div>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content={`${SITE_NAME}: documentation`} />
+      <meta name="og:title" content={`${SITE_NAME}: documentation`} />
+    </>
+  ),
+  gitTimestamp: 'Last updated on',
+  defaultMenuCollapsed: true,
+  feedbackLink: 'Question? Give us feedback →',
+  feedbackLabels: 'kind/docs',
+};
+
+export default config;
