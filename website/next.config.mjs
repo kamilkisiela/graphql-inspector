2bcb24956aa4d3a1e523e97992230692fb3e34f0
Dotan Simha <dotansimha@gmail.com>
2022-09-05 20:03:21 +0300

    fixes for videos (#2168)

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,28 +1,31 @@
 import { withGuildDocs } from 'guild-docs/next.config';
 import { applyUnderscoreRedirects } from 'guild-docs/underscore-redirects';
+import withVideos from 'next-videos';
 
-export default withGuildDocs({
-  basePath: process.env.NEXT_BASE_PATH && process.env.NEXT_BASE_PATH !== '' ? process.env.NEXT_BASE_PATH : undefined,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  swcMinify: true,
-  experimental: {
-    images: {
-      unoptimized: true,
-      allowFutureImage: true,
+export default withGuildDocs(
+  withVideos({
+    basePath: process.env.NEXT_BASE_PATH && process.env.NEXT_BASE_PATH !== '' ? process.env.NEXT_BASE_PATH : undefined,
+    eslint: {
+      ignoreDuringBuilds: true,
     },
-  },
-  webpack(config, meta) {
-    applyUnderscoreRedirects(config, meta);
+    swcMinify: true,
+    experimental: {
+      images: {
+        unoptimized: true,
+        allowFutureImage: true,
+      },
+    },
+    webpack(config, meta) {
+      applyUnderscoreRedirects(config, meta);
 
-    return config;
-  },
-  redirects: () => [
-    {
-      source: '/install',
-      destination: 'https://github.com/apps/graphql-inspector',
-      permanent: true,
+      return config;
     },
-  ],
-});
+    redirects: () => [
+      {
+        source: '/install',
+        destination: 'https://github.com/apps/graphql-inspector',
+        permanent: true,
+      },
+    ],
+  })
+);
