75dd6610392c2dcb8f4018e2d6ffa9994808046f
Dotan Simha <dotansimha@gmail.com>
2022-09-05 18:52:26 +0300

    fix

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,5 @@
 import { withGuildDocs } from 'guild-docs/next.config';
+import { applyUnderscoreRedirects } from 'guild-docs/underscore-redirects';
 
 export default withGuildDocs({
   basePath: process.env.NEXT_BASE_PATH && process.env.NEXT_BASE_PATH !== '' ? process.env.NEXT_BASE_PATH : undefined,
