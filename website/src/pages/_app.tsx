54140fc1edc602ac52810e6573c8db4808f43391
Dotan Simha <dotansimha@gmail.com>
2022-09-05 18:47:04 +0300

    Website adjustments to use a unified domain

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,16 +1,10 @@
-import Script from 'next/script';
 import { AppProps } from 'next/app';
 import { FooterExtended, Header, ThemeProvider } from '@theguild/components';
-import { useGoogleAnalytics } from 'guild-docs';
 import 'guild-docs/style.css';
 
 export default function App({ Component, pageProps, router }: AppProps) {
-  const analytics = useGoogleAnalytics({ router, trackingId: 'G-HQTC95CPEL' });
   return (
     <ThemeProvider>
-      <Script async src="https://the-guild.dev/static/crisp.js" />
-      <Script {...analytics.loadScriptProps} />
-      <Script {...analytics.configScriptProps} />
       <Header accentColor="#1cc8ee" themeSwitch searchBarProps={{ version: 'v2' }} />
       <Component {...pageProps} />
       <FooterExtended />
