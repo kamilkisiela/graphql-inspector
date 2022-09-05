c1670048eb70408cebe48f828995827812476b6e
Dotan Simha <dotansimha@gmail.com>
2022-09-05 19:14:21 +0300

    Website adjustments to use a unified domain (#2165)

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,10 +1,23 @@
 import { withGuildDocs } from 'guild-docs/next.config';
+import { applyUnderscoreRedirects } from 'guild-docs/underscore-redirects';
 
 export default withGuildDocs({
+  basePath: process.env.NEXT_BASE_PATH && process.env.NEXT_BASE_PATH !== '' ? process.env.NEXT_BASE_PATH : undefined,
   eslint: {
     ignoreDuringBuilds: true,
   },
   swcMinify: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+      allowFutureImage: true,
+    },
+  },
+  webpack(config, meta) {
+    applyUnderscoreRedirects(config, meta);
+
+    return config;
+  },
   redirects: () => [
     {
       source: '/install',
