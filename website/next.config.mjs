bf589291227827b5599820f5c83b0bef58d7f9d4
Dotan Simha <dotansimha@gmail.com>
2022-10-03 11:03:22 +0700

    Update website setup to latest (#2185)

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,49 +1,29 @@
-import { withGuildDocs } from 'guild-docs/next.config';
-import { applyUnderscoreRedirects } from 'guild-docs/underscore-redirects';
-import withVideos from 'next-videos';
+import { withGuildDocs } from '@theguild/components/next.config';
 
-export default withGuildDocs(
-  withVideos({
-    basePath: process.env.NEXT_BASE_PATH && process.env.NEXT_BASE_PATH !== '' ? process.env.NEXT_BASE_PATH : undefined,
-    experimental: {
-      images: {
-        unoptimized: true, // doesn't work with `next export`
-        allowFutureImage: true,
-      },
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    swcMinify: true,
-    experimental: {
-      images: {
-        unoptimized: true,
-        allowFutureImage: true,
-      },
-    },
-    webpack(config, meta) {
-      applyUnderscoreRedirects(config, meta);
-
-      return config;
-    },
-    redirects: () =>
-      Object.entries({
-        '/install': '/docs/introduction/installation',
-        '/docs': '/docs/introduction',
-        '/enterprise': '/docs/introduction',
-        '/docs/installation': '/docs/introduction/installation',
-        '/docs/index': '/docs/introduction',
-        '/products': '/docs/products/ci',
-        '/docs/recipies/github': '/docs/products/github',
-        '/docs/installation': '/docs/introduction/installation',
-        '/docs/api': '/docs/api/schema',
-        '/docs/recipes': '/docs/recipes/environments',
-        '/docs/recipes/github': '/docs/recipes/pull-requests',
-        '/docs/essentials': '/docs/essentials/diff',
-      }).map(([from, to]) => ({
-        source: from,
-        destination: to,
-        permanent: true,
-      })),
-  })
-);
+export default withGuildDocs({
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true, // doesn't work with `next export`
+  },
+  redirects: () =>
+    Object.entries({
+      '/install': '/docs/introduction/installation',
+      '/docs': '/docs/introduction',
+      '/enterprise': '/docs/introduction',
+      '/docs/installation': '/docs/introduction/installation',
+      '/docs/index': '/docs/introduction',
+      '/products': '/docs/products/ci',
+      '/docs/recipies/github': '/docs/products/github',
+      '/docs/installation': '/docs/introduction/installation',
+      '/docs/api': '/docs/api/schema',
+      '/docs/recipes': '/docs/recipes/environments',
+      '/docs/recipes/github': '/docs/recipes/pull-requests',
+      '/docs/essentials': '/docs/essentials/diff',
+    }).map(([from, to]) => ({
+      source: from,
+      destination: to,
+      permanent: true,
+    })),
+});
--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,23 +1,7 @@
+import { ReactElement } from 'react';
 import { AppProps } from 'next/app';
-import { FooterExtended, Header, ThemeProvider } from '@theguild/components';
-import 'guild-docs/style.css';
+import '@theguild/components/style.css';
 
-export default function App({ Component, pageProps, router }: AppProps) {
-  return (
-    <ThemeProvider>
-      <Header accentColor="#1cc8ee" themeSwitch searchBarProps={{ version: 'v2' }} />
-      <Component {...pageProps} />
-      <FooterExtended />
-    </ThemeProvider>
-  );
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />;
 }
-
-// const defaultSeo: AppSeoProps = {
-//   title: 'GraphQL Inspector',
-//   description: `Validate schemas and detect changes. Receive schema change notifications. Keep Operations and Fragments consistent.`,
-//   logo: {
-//     url: 'https://graphql-inspector.com/assets/subheader-logo.png',
-//     width: 50,
-//     height: 54,
-//   },
-// };
