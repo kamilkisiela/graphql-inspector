2a616f833b14a6ea0d4b5e3d528fd5b4cedd4140
Tuval Simha <tuval.simha@Gmail.com>
2023-01-02 17:15:37 +0200

    Update packages/core/src/utils/graphql.ts
    Co-authored-by: Dimitri POSTOLOV <dmytropostolov@gmail.com>

--- packages/core/src/utils/graphql.ts
+++ packages/core/src/utils/graphql.ts
@@ -167,7 +167,7 @@ export function findDeprecatedUsages(schema: GraphQLSchema, ast: DocumentNode): 
 }
 
 export function removeFieldIfDirectives(node: FieldNode, directiveNames: string[]): FieldNode | null {
-  if (node.directives && node.directives.some(d => directiveNames.includes(d.name.value))) {
+  if (node.directives?.some(d => directiveNames.includes(d.name.value))) {
       return null;
     }
 
