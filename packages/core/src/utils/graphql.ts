25f289f99ddb6a4bf9284adf9e9885752a180d8b
Tuval Simha <tuval.simha@gmail.com>
2023-01-04 11:35:45 +0200

    unicorn/no-lonely-if
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>

--- packages/core/src/utils/graphql.ts
+++ packages/core/src/utils/graphql.ts
@@ -167,11 +167,9 @@ export function findDeprecatedUsages(schema: GraphQLSchema, ast: DocumentNode): 
 }
 
 export function removeFieldIfDirectives(node: FieldNode, directiveNames: string[]): FieldNode | null {
-  if (node.directives) {
-    if (node.directives.some(d => directiveNames.includes(d.name.value))) {
+  if (node.directives && node.directives.some(d => directiveNames.indexOf(d.name.value) !== -1)) {
       return null;
     }
-  }
 
   return node;
 }
