20cf7e0e97d5602bc3bdbd080d7757d8907d1136
Laurin Quast <laurinquast@googlemail.com>
2023-03-29 10:56:30 +0200

    fix: breaking language features

--- packages/github/src/helpers/config.ts
+++ packages/github/src/helpers/config.ts
@@ -206,12 +206,19 @@ export function createConfig(
   }
 
   for (const branch of branches) {
-    config = config || findConfigByBranch(branch, normalizedConfig, false);
+    if (config == null) {
+      config = findConfigByBranch(branch, normalizedConfig, false);
+    }
+
     if (config) {
       break;
     }
   }
-  config = config || getGlobalConfig(rawConfig as any, fallbackBranch);
+
+  if (config == null) {
+    config = getGlobalConfig(rawConfig as any, fallbackBranch);
+  }
+
   return config;
 }
 
