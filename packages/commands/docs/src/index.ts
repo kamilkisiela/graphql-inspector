fa585d483f66804286761d6f55ec6e3fb5a1379c
TuvalSimha <tuval.simha@gmail.com>
2023-04-16 16:26:45 +0300

    fix open
    Signed-off-by: TuvalSimha <tuval.simha@gmail.com>

--- packages/commands/docs/src/index.ts
+++ packages/commands/docs/src/index.ts
@@ -5,8 +5,8 @@ export default createCommand((): any => {
   return {
     command: ['docs', 'website'],
     describe: 'Open Documentation',
-    handler() {
-      return open('https://graphql-inspector.com');
+    async handler() {
+      return await open('https://graphql-inspector.com');
     },
   };
 });
