525e9acde850aead2add19a4c5039681fdd2caa1
TuvalSimha <tuval.simha@gmail.com>
2023-04-20 16:33:07 +0300

    bug fix `serve`
    Signed-off-by: TuvalSimha <tuval.simha@gmail.com>

--- packages/commands/serve/src/index.ts
+++ packages/commands/serve/src/index.ts
@@ -42,9 +42,17 @@ export default createCommand<
     },
     async handler(args) {
       const { headers, token } = parseGlobalArgs(args);
+      console.log("args", args);
+      console.log("headers", headers);
+      console.log("token", token);
+
       const apolloFederation = args.federation || false;
+      console.log("apolloFederation", apolloFederation);
+
       const aws = args.aws || false;
+      console.log("aws", aws);
       const method = args.method?.toUpperCase() || 'POST';
+      console.log("method", method);
 
       const schema = await loaders.loadSchema(
         args.schema,
@@ -57,7 +65,10 @@ export default createCommand<
         aws,
       );
 
+
       const port = args.port;
+      console.log("port", port);
+
 
       try {
         fake(schema);
@@ -66,13 +77,20 @@ export default createCommand<
           schema,
           logging: false,
         });
+        console.log("yoga", yoga);
+
 
         const server = createServer(yoga);
+        console.log("server", server);
+
 
         await new Promise<void>(resolve => server.listen(port, () => resolve()));
+        console.log("port", port);
 
         const url = `http://localhost:${port}/graphql`;
+        console.log("url", url);
         Logger.success(`GraphQL API:    ${url}`);
+        
         await open(url);
 
         const shutdown = () => {
