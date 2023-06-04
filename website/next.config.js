9ba0248ffe038f5a6c5abc26ca0acacf75d54982
Tuval Simha <tuval.simha@Gmail.com>
2023-06-04 19:01:08 +0300

    Website: fix redirects (#2528)

--- website/next.config.js
+++ website/next.config.js
@@ -7,16 +7,9 @@ export default withGuildDocs({
   redirects: () =>
     Object.entries({
       '/install': '/docs/introduction/installation',
-      '/docs': '/docs/introduction',
       '/enterprise': '/docs/introduction',
-      '/docs/installation': '/docs/introduction/installation',
-      '/docs/index': '/docs/introduction',
+      '/docs/index': '/docs',
       '/products': '/docs/products/ci',
-      '/docs/recipies/github': '/docs/products/github',
-      '/docs/api': '/docs/api/schema',
-      '/docs/recipes': '/docs/recipes/environments',
-      '/docs/recipes/github': '/docs/recipes/pull-requests',
-      '/docs/essentials': '/docs/essentials/diff',
     }).map(([from, to]) => ({
       source: from,
       destination: to,
