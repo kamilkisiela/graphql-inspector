cc9c44e7bd16a26cdfadd6858a6437c15a2bc310
TuvalSimha <tuval.simha@gmail.com>
2023-07-19 16:25:41 +0300

    Fix CLI build check

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-import yargs, { Argv } from 'yargs';
+import { Argv } from 'yargs';
 import { useCommands } from '@graphql-inspector/commands';
 import { useLoaders } from '@graphql-inspector/loaders';
 
 async function main() {
+  const yargs = require('yargs');
   const config = {
     loaders: ['code', 'git', 'github', 'graphql', 'json', 'url'],
     commands: ['docs', 'serve', 'diff', 'validate', 'coverage', 'introspect', 'similar', 'audit'],
