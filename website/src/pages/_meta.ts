fb21c9a8363546b0c5fa7de9525f2377ab767568
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-09 00:35:37 +0200

    update to nextra v3

---
+++ website/src/pages/_meta.ts
@@ -0,0 +1,23 @@
+export default {
+  index: {
+    title: 'Home',
+    type: 'page',
+    display: 'hidden',
+    theme: {
+      layout: 'raw',
+    },
+  },
+  docs: {
+    title: 'Docs',
+    type: 'page',
+    theme: {
+      toc: true,
+    },
+  },
+  hive: {
+    title: 'Enterprise',
+    type: 'page',
+    href: 'https://graphql-hive.com',
+    newWindow: true,
+  },
+};
