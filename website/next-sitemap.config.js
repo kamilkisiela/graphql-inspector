bdb8a1c2e3c62e8980af1d837218bfa5ac8b5303
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-04-11 17:17:27 +0200

    update components (#2711)

--- website/next-sitemap.config.js
+++ website/next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
+export default {
   siteUrl: process.env.SITE_URL || 'https://the-guild.dev/graphql/inspector',
   generateIndexSitemap: false,
   exclude: ['*/_meta'],
