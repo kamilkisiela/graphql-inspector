eabe6c52f20992e3b6525609f0989b4874c018c4
TuvalSimha <tuval.simha@gmail.com>
2024-05-26 16:19:55 +0300

    update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -44,4 +44,10 @@ module.exports = {
       },
     },
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
 };
